28ec0c838cec78e943123bf8c7e9c017ef97b8a4
anymen331 <anymen331@gmail.com>
2025-02-01 02:38:29 +0000

    Agregue codigo en app.js para el generador de dominios

--- src/app.js
+++ src/app.js
@@ -8,4 +8,59 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const excusaElement = document.getElementById("excusa");
+  const pronoun = [
+    "the",
+    "our",
+    "theirs",
+    "his",
+    "its",
+    "this",
+    "that",
+    "mine",
+    "your",
+    "them"
+  ];
+  const adj = [
+    "great",
+    "big",
+    "small",
+    "fast",
+    "slow",
+    "happy",
+    "sad",
+    "beautiful",
+    "interesting",
+    "delicious"
+  ];
+  const noun = [
+    "jogger",
+    "racoon",
+    "tree",
+    "car",
+    "house",
+    "book",
+    "dog",
+    "cat",
+    "city",
+    "mountain"
+  ];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+
+  // function GenerandoExcusas() {
+  //   const randomProun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  //   const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  //   const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+
+  //   const excusa = randomProun + randomAdj + randomNoun + ".com";
+  //   return excusa;
+  // }
+  // excusaElement.textContent = GenerandoExcusas();
 };
